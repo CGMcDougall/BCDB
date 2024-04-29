ecb444ef2e0cc94fc38ddb95df80d275823bf378
CG McDougall <ConnorMcDougall29@outlook.com>
2024-04-29 14:11:33 -0400

    Added SQLManager and a locInfo classFile

---
+++ src/main/java/SQLManager.java
@@ -0,0 +1,7 @@
+
+public class SQLManager {
+
+    String dbUname = locInfo.dbUname;
+    String dbPass = locInfo.dbPass;
+
+}
---
+++ src/main/java/locInfo.java
@@ -0,0 +1,8 @@
+//Should be in the gitIgnore
+
+public class locInfo {
+
+    static String dbUname = "";
+    static String dbPass = "";
+
+}
